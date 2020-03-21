ceea65a2294bba40d146388711ddf4844565dc9e
Brett <60373457+Bkearney1@users.noreply.github.com>
2020-03-21 19:18:35 -0400

    Add files via upload
    now returns JSON string
    added GsonFormat class

--- prototype/src/main/java/Main.java
+++ prototype/src/main/java/Main.java
@@ -9,13 +9,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scraper example = new Scraper();
         //Change the ticker to whatever you want
-        String ticker = "AAPL";
+        String ticker = "GOOGL";
         System.out.println("Description: " + example.getDescription(ticker));
         System.out.println("Current Price: " + example.getCurrentPrice(ticker));
-        example.getHistoricalData("APPL","BrandNew",5);
-        List <BigDecimal> priceHistory = example.getOpeningPriceHistory("BrandNew");
-        for(BigDecimal item : priceHistory){
-            System.out.println(item);
-        }
+
+
+        System.out.println(example.getJson("GOOGL"));
+
+
+        //example.getHistoricalData("GOOGL","priceHistory",5);
+        //List <BigDecimal> priceHistory = example.getOpeningPriceHistory("priceHistory");
+        //print out values in priceHistory
+        //for(BigDecimal item : priceHistory){
+        //    System.out.println(item);
+        //}
     }
 }
--- prototype/src/main/java/Scraper.java
+++ prototype/src/main/java/Scraper.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
 import java.math.BigDecimal;
+import java.net.MalformedURLException;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.StandardCharsets;
@@ -9,6 +10,13 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
 import org.jsoup.Jsoup;
 
 import org.jsoup.nodes.Document;
@@ -18,9 +26,7 @@ import org.jsoup.select.Elements;
 
 
 public class Scraper {
-    public Scraper(){
 
-    }
 
     public String getDescription(String tickerSymbol) throws IOException {
         String URL = "https://finance.yahoo.com/quote/"+tickerSymbol+"/profile?p="+tickerSymbol;
@@ -36,64 +42,41 @@ public class Scraper {
         return new BigDecimal(e.text().replace(",",""));
     }
 
-    //gets a CSV file of the ticker on a MONTHLY basis
-    //accepts the ticker symbol, an output filename, and the number of years to find
-    public  void getHistoricalData(String ticker, String outputFileName, int numYears) throws IOException {
+
+
+    public String getJson(String ticker) throws IOException {
         long unixTime = System.currentTimeMillis() / 1000L;
         //calculate seconds passed & sub from current unix time
-        long oldUnixTime = unixTime - (numYears*365*24*60*60);
-       URL url = new URL ("https://query1.finance.yahoo.com/v7/finance/download/"+ticker+"?period1="+oldUnixTime+"&period2="+unixTime+"&interval=1mo&events=history");
-       InputStream stream = url.openStream();
-       try {
-           FileOutputStream fos = new FileOutputStream(new File(outputFileName));
-           int length = -1;
-           byte[] buffer = new byte[1024];// buffer for portion of data from connection
-           while ((length = stream.read(buffer)) > -1) {
-               fos.write(buffer, 0, length);
-           }
-           fos.close();
-           stream.close();
-           System.out.println("done getting file");
-       }catch (IOException e){
-           System.out.println("Program Error: ");
-            System.out.println(e.toString());
-       }
-
-    }
-
-    public List getOpeningPriceHistory(String fileName) throws IOException {
-
-        File file = new File(fileName);
-        List <BigDecimal> priceData = new LinkedList<BigDecimal>();
-        List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
-        lines.remove(0);
-
-        for (String line : lines) {
-            String[] array = line.split(",", -1);
-            priceData.add(new BigDecimal(array[1]));
-        }
-
-        return priceData;
-    }
-
-
-    //Deprecated method that scrapes HTML instead of getting CSV file
-    public List <BigDecimal> getFiveYearMonthlyPriceHistory(String tickerSymbol) throws IOException{
-        long unixTime = System.currentTimeMillis() / 1000L;
         long oldUnixTime = unixTime - (5*365*24*60*60);
-        String URL = "https://finance.yahoo.com/quote/"+tickerSymbol+"/history?period1="+oldUnixTime+"&period2="+unixTime+"&interval=1mo&filter=history&frequency=1mo";
-        Document doc = Jsoup.connect(URL).get();
-        Elements x = doc.getElementsByClass("Py(10px) Pstart(10px)");
-        String s = x.text().replace(",","");
-        List<String> rawData = new LinkedList<String>(Arrays.asList(s.split(" ")));
-        List<BigDecimal> openingPrices = new LinkedList<>();
-        for(int i=0;i<rawData.size();i++) {
-            if (i % 6 == 0) {
-                openingPrices.add(new BigDecimal(rawData.get(i)));
-            }
+
+        String companyDescription = this.getDescription(ticker);
+
+        String webResponse = new Scanner(new URL("https://query1.finance.yahoo.com/v7/finance/download/"+ticker+"?period1="+oldUnixTime+"&period2="+unixTime+"&interval=1mo&events=history").openStream(), "UTF-8").useDelimiter("\\A").next();
+
+        List<String> rowsList =  new LinkedList<String>(Arrays.asList(webResponse.split("\\n")));
+        //gets rid of header
+        rowsList.remove(0);
+
+        List<String> col = new LinkedList<String>();
+
+        for(int i=0;i<rowsList.size();i++){
+            col.addAll(Arrays.asList(rowsList.get(i).split(",")));
         }
-        return openingPrices;
+        List <GsonFormat> formatted = new LinkedList<GsonFormat>();
+
+        for(int i=0; i<col.size()-6;i+=6){
+            formatted.add(new GsonFormat(col.get(i),col.get(i+1),col.get(i+2),col.get(i+3)  ,col.get(i+4),col.get(i+5),col.get(i+6)));
+        }
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(formatted);
+
+
+       return json;
     }
+
+
+
 
 
 
